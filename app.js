aba46f04e745461a51345f7e8fa9dbc60af4eff8
TuanAn <nptatdt@gmail.com>
2022-02-28 13:40:37 +0700

    api booking

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
+require('dotenv').config({ path: require('find-config')('.env') })
 const express = require('express');
+const axios = require('axios')
+const { Telegraf } = require('telegraf');
 const app = express();
+const port = 4000;
+const botToken = process.env.TELEGRAM_ACCESS_TOKEN
+const chatID = '5098641310'
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// var dataToSend = "Hello Mateeeeeeeeeeeee"
+// axios.post(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatID}&text=${dataToSend}`)
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/api/booking'
+    , (req, res) => {
+        const dataToSend = req.body;
+        console.log(dataToSend)
+        //axios.post(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatID}&text=${dataToSend}`);
+    })
 
 app.get('/api/test'
     , (req, res) => {
@@ -7,7 +31,7 @@ app.get('/api/test'
     }
 )
 
-app.listen(4000
+app.listen(port
     , () => {
-        console.log('App listening to port 4000')
+        console.log('App listening to port', port)
     })
